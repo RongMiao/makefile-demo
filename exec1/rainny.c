dbfdb29c9bee2a43b307915324de38240a4254e4
rongmiao_wang <rongm325@qq.com>
2023-07-16 08:41:44 -0700

    add exec1

---
+++ exec1/rainny.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+void rainny() {
+	printf("Rainny!!\n");
+}
---
+++ exec1/src/hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "foo.h"
+#include "smile.h"
+#include "rainny.h"
+void main()
+{
+    printf("HELLO WORLD!\n");
+	foo();
+	smile();
+	rainny();
+}
